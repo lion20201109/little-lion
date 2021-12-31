925d23756c8a1619c83b4187709331c74cfe3892
lion20201109 <jiangnannaruto@163.com>
2021-12-31 11:05:14 +0800

    init

---
+++ src/main/java/com/jyc/lion/xsz/XszApplication.java
@@ -0,0 +1,13 @@
+package com.jyc.lion.xsz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XszApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XszApplication.class, args);
+    }
+
+}
