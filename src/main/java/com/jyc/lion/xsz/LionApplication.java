2a8c86ea35d0645e34ec151e1f017bb6cc383444
lion20201109 <jiangnannaruto@163.com>
2021-12-31 11:06:49 +0800

    init

--- src/main/java/com/jyc/lion/xsz/LionApplication.java
+++ src/main/java/com/jyc/lion/xsz/LionApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class XszApplication {
+public class LionApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(XszApplication.class, args);
+        SpringApplication.run(LionApplication.class, args);
     }
 
 }
